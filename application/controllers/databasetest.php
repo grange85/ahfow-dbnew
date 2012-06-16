9dd96d900b109450000f92813f339915115f5067
grange85 <andy@grange85.co.uk>
2012-06-16 22:22:38 +0100

    created view wrrappers

--- application/controllers/databasetest.php
+++ application/controllers/databasetest.php
@@ -3,6 +3,8 @@
 
 class Databasetest extends CI_Controller {
     
+    private $head_data = array();
+    
     function __construct() {
         parent::__construct();
     }
@@ -12,7 +14,11 @@ class Databasetest extends CI_Controller {
         
         $this->load->model('Galaxiedb');
         $data['artists']=$this->Galaxiedb->get_artists();
+        $head_data['url']=  base_url();
+        $head_data['props']['title']='A Head Full of Wishes';
+        $this->load->view('wrapper/header', $head_data);
         $this->load->view('artists', $data);
+        $this->load->view('wrapper/footer');
         
         
     }
---
+++ application/views/wrapper/header.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title><?php echo $props['title'];?></title>
+        
+        
+    </head>
+    <body>
+        <h1><?php echo $url;?></h1>
