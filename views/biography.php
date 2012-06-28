74fb1fb7513605c3981daedd131bf68ced4a4d85
grange85 <andy@grange85.co.uk>
2012-06-28 23:59:40 +0100

    caching test

--- views/biography.php
+++ views/biography.php
@@ -14,22 +14,22 @@
 ?>
 
 <div id="content_left">
-    <h2><?php echo $artist_details->display . ': ' . $section; ?></h2>
+    <h2><?php echo htmlentities($artist_details->display) . ': ' . $section; ?></h2>
 
     <?php
     if ($artist_details->image) {
         $image_props = array(
             'src' => MEDIA_HOST . '/sleeves/' . $artist_details->image,
-            'alt' => $artist_details->display,
+            'alt' => htmlentities($artist_details->display),
             'width' => '200',
             'height' => '200',
-            'title' => $artist_details->display
+            'title' => htmlentities($artist_details->display)
         );
         ?>
 
         <div class="imagebox_right">
             <?php echo img($image_props); ?>
-            <p><?php echo $artist_details->display; ?></p>
+            <p><?php echo htmlentities($artist_details->display); ?></p>
         </div>
         <?php
     }
