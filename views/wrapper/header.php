b722b62a8fc79499088bcbe4f0323ffe304712b4
grange85 <andy@grange85.co.uk>
2012-07-08 14:51:19 +0100

    Added google analytics

--- views/wrapper/header.php
+++ views/wrapper/header.php
@@ -3,8 +3,8 @@
     <head>
         <title>A Head Full of Wishes: <?php echo $page_title; ?>
 
-        
-        
+
+
         </title>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
         <link rel="stylesheet" href="<?php echo STATIC_HOST; ?>/css/reset.css" type="text/css" />
@@ -14,7 +14,19 @@
         <script src="<?php echo STATIC_HOST; ?>/js/jquery.hoverIntent.minified.js" type="text/javascript"></script>
         <script src="<?php echo STATIC_HOST; ?>/js/ahfowdb-core.js" type="text/javascript"></script>
 
+        <script type="text/javascript">
 
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-386732-16']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
     </head>
 
     <body>
