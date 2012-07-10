56ac8e8387e78b12e0d8d4972754a2a4f68cae86
Andy Aldridge <andy@grange85.co.uk>
2012-07-10 11:01:20 +0200

    Update master

--- views/wrapper/header.php
+++ views/wrapper/header.php
@@ -13,22 +13,19 @@
         <script src="<?php echo STATIC_HOST; ?>/js/<?php echo JQUERY_LIBRARY; ?>" type="text/javascript"></script>
         <script src="<?php echo STATIC_HOST; ?>/js/jquery.hoverIntent.minified.js" type="text/javascript"></script>
         <script src="<?php echo STATIC_HOST; ?>/js/ahfowdb-core.js" type="text/javascript"></script>
+<script type="text/javascript">
 
-        <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-386732-6']);
+  _gaq.push(['_trackPageview']);
 
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-386732-5']);
-            _gaq.push(['_setDomainName', 'fullofwishes.co.uk']);
-            _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
-        </script>        
-
+</script>
 
     </head>
 
