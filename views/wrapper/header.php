5e03eb775c5bc45dba54dca3b5947bd78449d2dc
grange85 <andy@grange85.co.uk>
2013-03-11 23:43:53 +0000

    Some initial rubbishy responsive stuff

--- views/wrapper/header.php
+++ views/wrapper/header.php
@@ -7,6 +7,7 @@
 
         </title>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="<?php echo STATIC_HOST; ?>/css/reset.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo STATIC_HOST; ?>/css/core.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo STATIC_HOST; ?>/css/survey.css" type="text/css" />
@@ -25,36 +26,40 @@
             _gaq.push(['_trackPageview']);
 
             (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
                 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
             })();
 
-            
-            document.domain = window.location.hostname.replace('db.','');
+
+            document.domain = window.location.hostname.replace('db.', '');
             console.log(document.domain);
             tinyMCE.init({
-                language : 'en',
-                mode : 'specific_textareas',
-                editor_selector : 'mceEditor',
-                theme : 'advanced',
-                theme_advanced_buttons1 : "bold,italic,underline,|,link,unlink,|,bullist,numlist,|,undo,redo,|,removeformat,code",
-                theme_advanced_toolbar_location : "top",
-                theme_advanced_toolbar_align : "center",
-                theme_advanced_statusbar_location : "bottom",
-                theme_advanced_resizing : true,
-                theme_advanced_font_sizes : "10px,12px,14px,16px,24px"
-        
+                language: 'en',
+                mode: 'specific_textareas',
+                editor_selector: 'mceEditor',
+                theme: 'advanced',
+                theme_advanced_buttons1: "bold,italic,underline,|,link,unlink,|,bullist,numlist,|,undo,redo,|,removeformat,code",
+                theme_advanced_toolbar_location: "top",
+                theme_advanced_toolbar_align: "center",
+                theme_advanced_statusbar_location: "bottom",
+                theme_advanced_resizing: true,
+                theme_advanced_font_sizes: "10px,12px,14px,16px,24px"
+
             });
 
             (function() {
                 var cx = '017452044498352075094:txisiezhclu';
-                var gcse = document.createElement('script'); 
-                gcse.type = 'text/javascript'; 
+                var gcse = document.createElement('script');
+                gcse.type = 'text/javascript';
                 gcse.async = true;
                 gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-                    '//www.google.com/cse/cse.js?cx=' + cx;
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+                        '//www.google.com/cse/cse.js?cx=' + cx;
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(gcse, s);
             })();
 
         </script>
