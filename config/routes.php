528ee8b32b35ba22ae9e3d022ccc2f426dc7213d
Andy Aldridge <andy@grange85.co.uk>
2013-03-12 12:29:37 +0000

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -21,8 +21,7 @@
 */
 
 $route['gigography/show/([0-9]+)'] = 'database/gigography/show/$1';
-$route['survey/([0-9]{4})'] = 'survey/view/$1';
-$route['survey/([0-9]{4})/(.+)'] = 'survey/view/$1#$2';
+$route['survey/([0-9]{4})(\/.*)'] = 'survey/view/$1';
 
 /*
 |
